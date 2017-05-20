b52649a912f9f3bf0c3e0ac3ff0d7c74d9086cfb
ataneo <atanuknath@gmail.com>
2017-05-19 22:44:43 -0700

    Second commit

--- index.js
+++
@@ -1,6 +0,0 @@
-//alert("Hello1");
-
-
-
-var a = 10;
-(function(){ alert("Hello1") })();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+//alert("Hello1");
+
+
+
+var a = 10;
+(function(){ alert("Monika I love you") })();
+
+
+b=2323;
+
+alert(b);
+
